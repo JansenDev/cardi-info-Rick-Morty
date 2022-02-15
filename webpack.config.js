db47a84b45205fe0078ca9248775beb879fcfb01
Jhonatan <seguragjj25@gmail.com>
2022-02-15 12:54:46 -0500

    ADDED: babel loader(.babelrc, webpack.config.js)

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,28 @@
 const path = require("path");
 
 module.exports = {
+  //^ configuracion básica
+  // *  index principal que da inicio a la aplicacion
   entry: "./src/index.js",
+  //   *Salida del blundle
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "main.js",
   },
+  //   *archivos que va a resolver en la aplicacion
   resolve: {
     extensions: [".js"],
+  },
+  module: {
+    // *  Establece reglas de modulo
+    rules: [
+      {
+        test: /.\m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
   },
 };
