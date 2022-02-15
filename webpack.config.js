b6f3e0d80d04d326f88ecd5c5d1c5d6b6c8013b9
Jhonatan <seguragjj25@gmail.com>
2022-02-15 16:45:56 -0500

    ADDED: config para las font y salida general de los assets

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "main.js",
+    assetModuleFilename: "assets/images/[hash][ext][query]",
   },
   //   *archivos que va a resolver en la aplicacion
   resolve: {
@@ -38,9 +39,23 @@ module.exports = {
       // ^ejmpl: import UserNotFound from "assets/myImage.png";
       // !Viene en webpack solo hay que configurarla
       {
-        test:/\.png/,
-        type:"asset/resource"
-      }
+        test: /\.png/,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2)$/,
+        use: {
+          loader: "url-loader",
+          options: {
+            limit: 10000,
+            mimetype: "application/font-woff",
+            name: "[name].[ext]",
+            outputPath: "./assets/fonts/",
+            publicPath: "./assets/fonts/",
+            esModule: false,
+          },
+        },
+      },
     ],
   },
   // ^Agregando pluggin para renderizar HTML
